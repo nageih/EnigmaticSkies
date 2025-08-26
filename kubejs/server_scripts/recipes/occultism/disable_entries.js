e57d464cd586f1aeff6b7738b33df465e9dd2747
MuteTiefling <mutetiefling@gmail.com>
2025-08-25 22:54:48 -0400

    vexing update, occultism pentacle rework

---
+++ kubejs/server_scripts/recipes/occultism/disable_entries.js
@@ -0,0 +1,35 @@
+ServerEvents.generateData('after_mods', (event) => {
+    let entries = [
+        { name: 'didactics', section: 'pentacles' },
+        { name: 'contact_wild_spirit', section: 'pentacles' },
+        { name: 'craft_afrit', section: 'pentacles' },
+        { name: 'craft_djinni', section: 'pentacles' },
+        { name: 'craft_foliot', section: 'pentacles' },
+        { name: 'craft_marid', section: 'pentacles' },
+        { name: 'possess_afrit', section: 'pentacles' },
+        { name: 'possess_djinni', section: 'pentacles' },
+        { name: 'possess_foliot', section: 'pentacles' },
+        { name: 'possess_marid', section: 'pentacles' },
+        { name: 'possess_unbound_afrit', section: 'pentacles' },
+        { name: 'resurrect_spirit', section: 'pentacles' },
+        { name: 'summon_afrit', section: 'pentacles' },
+        { name: 'summon_djinni', section: 'pentacles' },
+        { name: 'summon_foliot', section: 'pentacles' },
+        { name: 'summon_marid', section: 'pentacles' },
+        { name: 'summon_unbound_afrit', section: 'pentacles' },
+        { name: 'summon_unbound_marid', section: 'pentacles' }
+    ];
+
+    colors.forEach((color) => {
+        entries.push({
+            name: `${color}_chalk`,
+            section: 'pentacles'
+        });
+    });
+
+    entries.forEach((entry) => {
+        event.json(`occultism:modonomicon/books/dictionary_of_spirits/entries/${entry.section}/${entry.name}`, {
+            'neoforge:conditions': [{ type: 'neoforge:never' }]
+        });
+    });
+});
