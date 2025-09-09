f9935428ccf909b8743c1fe79fefae85a2b32302
MuteTiefling <mutetiefling@gmail.com>
2025-09-09 10:09:49 -0400

    cleanup

--- kubejs/server_scripts/recipes/modern_industrialization/packer.js
+++ kubejs/server_scripts/recipes/modern_industrialization/packer.js
@@ -3,64 +3,84 @@ ServerEvents.recipes((event) => {
 
     const recipes = [
         {
+            item_outputs: [{ item: 'justdirethings:coalblock_t1', amount: 1 }],
             item_inputs: [
                 { item: 'justdirethings:coal_t1', amount: 9 },
                 { item: 'modern_industrialization:packer_block_template', amount: 1, probability: 0.0 }
             ],
-            item_outputs: [{ item: 'justdirethings:coalblock_t1', amount: 1 }],
             duration: 10,
             eu: 2,
             id: `${id_prefix}coal_t1`
         },
         {
+            item_outputs: [{ item: 'justdirethings:coalblock_t2', amount: 1 }],
             item_inputs: [
                 { item: 'justdirethings:coal_t2', amount: 9 },
                 { item: 'modern_industrialization:packer_block_template', amount: 1, probability: 0.0 }
             ],
-            item_outputs: [{ item: 'justdirethings:coalblock_t2', amount: 1 }],
             duration: 10,
             eu: 2,
             id: `${id_prefix}coal_t2`
         },
         {
+            item_outputs: [{ item: 'justdirethings:coalblock_t3', amount: 1 }],
             item_inputs: [
                 { item: 'justdirethings:coal_t3', amount: 9 },
                 { item: 'modern_industrialization:packer_block_template', amount: 1, probability: 0.0 }
             ],
-            item_outputs: [{ item: 'justdirethings:coalblock_t3', amount: 1 }],
             duration: 10,
             eu: 2,
             id: `${id_prefix}coal_t3`
         },
         {
+            item_outputs: [{ item: 'justdirethings:coalblock_t4', amount: 1 }],
             item_inputs: [
                 { item: 'justdirethings:coal_t4', amount: 9 },
                 { item: 'modern_industrialization:packer_block_template', amount: 1, probability: 0.0 }
             ],
-            item_outputs: [{ item: 'justdirethings:coalblock_t4', amount: 1 }],
             duration: 10,
             eu: 2,
             id: `${id_prefix}coal_t4`
         },
         {
+            item_outputs: [{ item: 'appflux:printed_energy_processor', amount: 1 }],
             item_inputs: [
                 { item: 'appflux:charged_redstone', amount: 1 },
                 { item: 'appflux:energy_processor_press', amount: 1, probability: 0.0 }
             ],
-            item_outputs: [{ item: 'appflux:printed_energy_processor', amount: 1 }],
             duration: 10,
             eu: 8,
             id: `${id_prefix}printed_energy_processor`
         },
         {
+            item_outputs: [{ item: 'advanced_ae:printed_quantum_processor', amount: 1 }],
             item_inputs: [
                 { tag: 'c:ingots/quantum_alloy', amount: 1 },
                 { item: 'advanced_ae:quantum_processor_press', amount: 1, probability: 0.0 }
             ],
-            item_outputs: [{ item: 'advanced_ae:printed_quantum_processor', amount: 1 }],
             duration: 10,
             eu: 8,
             id: `${id_prefix}printed_quantum_processor`
+        },
+        {
+            item_outputs: [{ item: 'modern_industrialization:steel_assembler', amount: 1 }],
+            item_inputs: [
+                { item: 'modern_industrialization:bronze_assembler', amount: 1 },
+                { item: 'modern_industrialization:steel_upgrade', amount: 1 }
+            ],
+            duration: 5,
+            eu: 2,
+            id: `${id_prefix}steel_assembler`
+        },
+        {
+            item_outputs: [{ item: 'modern_industrialization:steel_centrifuge', amount: 1 }],
+            item_inputs: [
+                { item: 'modern_industrialization:bronze_centrifuge', amount: 1 },
+                { item: 'modern_industrialization:steel_upgrade', amount: 1 }
+            ],
+            duration: 5,
+            eu: 2,
+            id: `${id_prefix}steel_centrifuge`
         }
     ];
 
