9b2bfa52a0a25493ef9014efdf7ea70842146314
MuteTiefling <mutetiefling@gmail.com>
2025-09-29 11:50:05 -0400

    leaky boiler

--- kubejs/server_scripts/recipes/mi_tweaks/dilapidated_boiler.js
+++ kubejs/server_scripts/recipes/mi_tweaks/dilapidated_boiler.js
@@ -4,28 +4,29 @@ ServerEvents.recipes((event) => {
     const recipes = [
         {
             item_inputs: { tag: 'c:gems/charcoal', amount: 1 },
-            output: 16000,
+            output: 8,
             id: `${id_prefix}steam_from_charcoal`
         },
         {
             item_inputs: [{ tag: 'enigmatica:planks', amount: 1 }],
-            output: 3000,
+            output: 1.5,
             id: `${id_prefix}steam_from_planks`
         },
         {
             item_inputs: [{ tag: 'c:tiny_gems/charcoal', amount: 1 }],
-            output: 2000,
+            output: 1,
             id: `${id_prefix}steam_from_tiny_charcoal`
         },
         {
             item_inputs: [{ tag: 'c:rods/wooden', amount: 1 }],
-            output: 1000,
+            output: 0.5,
             id: `${id_prefix}steam_from_sticks`
         }
     ];
 
     recipes.forEach((recipe) => {
         let rate = 8;
+        recipe.output *= 1000;
 
         recipe.type = 'modern_industrialization:dilapidated_boiler';
         recipe.fluid_outputs = [{ fluid: 'modern_industrialization:steam', amount: recipe.output }];
