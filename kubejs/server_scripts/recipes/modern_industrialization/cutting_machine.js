c6e7d2d705d89f725f3819961627476de2c41451
MuteTiefling <mutetiefling@gmail.com>
2025-10-19 16:59:35 -0400

    start cleaning up quests, easier electrostatic

--- kubejs/server_scripts/recipes/modern_industrialization/cutting_machine.js
+++ kubejs/server_scripts/recipes/modern_industrialization/cutting_machine.js
@@ -1,10 +1,21 @@
 ServerEvents.recipes((event) => {
     const id_prefix = 'enigmatica:modern_industrialization/cutting_machine/';
 
-    const recipes = [];
+    const recipes = [
+        {
+            item_outputs: [{ item: 'pneumaticcraft:aphorism_tile', amount: 4 }],
+            item_inputs: [{ item: 'naturesaura:infused_stone', amount: 1 }],
+            fluid_inputs: [{ fluid: 'modern_industrialization:lubricant', amount: 1 }],
+            duration: 5,
+            eu: 2,
+            id: `${id_prefix}aphorism_tile`
+        }
+    ];
 
     recipes.forEach((recipe) => {
         recipe.type = 'modern_industrialization:cutting_machine';
+        recipe.duration *= 20;
+
         event.custom(recipe).id(recipe.id);
 
         if (debug) console.log(recipe.id);
