77de27110349acf12b584d8821a02aae3756c159
MuteTiefling <mutetiefling@gmail.com>
2025-12-01 16:34:11 -0500

    prevent karki pickup in soul gems since it breaks them

---
+++ kubejs/server_scripts/tags/entity_type/occultism/fragile_soul_gem_deny_list.js
@@ -0,0 +1,3 @@
+ServerEvents.tags('entity_type', (event) => {
+    event.get('occultism:fragile_soul_gem_deny_list').add(['#occultism:soul_gem_deny_list']);
+});
--- kubejs/server_scripts/tags/entity_type/occultism/soul_gem_deny_list.js
+++ kubejs/server_scripts/tags/entity_type/occultism/soul_gem_deny_list.js
@@ -2,5 +2,10 @@ ServerEvents.tags('entity_type', (event) => {
     event
         .get('occultism:soul_gem_deny_list')
         .remove('minecraft:wither')
-        .add([/ars_(nouveau|elemental):.*familiar/, 'ars_nouveau:alakarkinos']);
+        .add([
+            /ars_(nouveau|elemental):.*familiar/,
+            'ars_nouveau:alakarkinos',
+            'ars_nouveau:wixie',
+            'ars_nouveau:starbuncle'
+        ]);
 });
