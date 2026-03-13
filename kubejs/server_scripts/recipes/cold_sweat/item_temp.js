acce7fd53f40f19481172f4185cd31871ce8393a
MuteTiefling <mutetiefling@gmail.com>
2026-03-13 08:11:53 -0400

    fix offhand temp, fix spellbook upgrade

--- kubejs/server_scripts/recipes/cold_sweat/item_temp.js
+++ kubejs/server_scripts/recipes/cold_sweat/item_temp.js
@@ -1,44 +1,47 @@
+// Item Requirements: https://mikul.gitbook.io/cold-sweat/datapacks/requirements/item-requirement
+// Item Config: https://mikul.gitbook.io/cold-sweat/datapacks/item-configs#item-temperature
+
 ServerEvents.generateData('before_mods', (event) => {
     const recipes = [
         {
             name: 'warm',
             item: { items: ['#enigmatica:warm'] },
-            slots: ['hotbar', 'inventory'],
+            slots: ['hand', 'hotbar', 'inventory'],
             temperature: 0.025,
             trait: 'core'
         },
         {
             name: 'hot',
             item: { items: ['#enigmatica:hot'] },
-            slots: ['hotbar', 'inventory'],
+            slots: ['hand', 'hotbar', 'inventory'],
             temperature: 1.0,
             trait: 'core'
         },
         {
             name: 'super_hot',
             item: { items: ['#enigmatica:super_hot'] },
-            slots: ['hotbar', 'inventory'],
+            slots: ['hand', 'hotbar', 'inventory'],
             temperature: 10.0,
             trait: 'core'
         },
         {
             name: 'chilled',
             item: { items: ['#enigmatica:chilled'] },
-            slots: ['hotbar', 'inventory'],
+            slots: ['hand', 'hotbar', 'inventory'],
             temperature: -0.005,
             trait: 'core'
         },
         {
             name: 'cold',
             item: { items: ['#enigmatica:cold'] },
-            slots: ['hotbar', 'inventory'],
+            slots: ['hand', 'hotbar', 'inventory'],
             temperature: -0.1,
             trait: 'core'
         },
         {
             name: 'super_cold',
             item: { items: ['#enigmatica:super_cold'] },
-            slots: ['hotbar', 'inventory'],
+            slots: ['hand', 'hotbar', 'inventory'],
             temperature: -0.5,
             trait: 'core'
         }
