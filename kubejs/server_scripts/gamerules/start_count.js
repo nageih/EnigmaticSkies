ec75bdcb76bfaab27644a537151e36b8bb052d03
MuteTiefling <mutetiefling@gmail.com>
2026-03-31 22:45:59 -0400

    safety first script

--- kubejs/server_scripts/gamerules/start_count.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.loaded((event) => {
-    const { server } = event;
-    let pData = server.persistentData;
-    if (!pData.getInt('start_count')) {
-        pData.putInt('start_count', 0);
-    }
-    pData.putInt('start_count', pData.getInt('start_count') + 1);
-});
